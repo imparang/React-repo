d923e66e70bb0e11439398525a6a39be55da1486
imparang <jeongjaeyn@gmail.com>
2021-12-05 17:56:37 +0900

    feat: Add SearchForm and BoardList component at App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
-import ProxyTest from './components/ProxyTest';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Container } from 'reactstrap'
+import BoardList from './components/BoardList'
+import SearchForm from './components/SearchForm'
+import { getBoardList } from './redux/reducers/board'
+// import ProxyTest from './components/ProxyTest';
 
 function App() {
+  const board = useSelector(state => state.board)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getBoardList())
+  }, [dispatch])
+
   return (
-    <div className="App">
-      <ProxyTest />
-    </div>
-  );
+    <Container>
+      <SearchForm />
+      <BoardList data={board.data} />
+    </Container>
+  )
 }
 
-export default App;
+export default App
